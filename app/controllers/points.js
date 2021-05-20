6ae154da0505a8618ca3a44d95000b5ac80cbcff
Leena <leena.web11@gmail.com>
2021-05-20 19:45:09 +0530

    implement drag point functionality

--- app/controllers/points.js
+++ app/controllers/points.js
@@ -6,6 +6,9 @@ import { inject as service } from '@ember/service';
 export default class PointsController extends Controller {
   @service store;
   @tracked allPoints = this.model;
+  @tracked map;
+  @tracked canvas;
+  @tracked hoverdPoint;
   @tracked markers = {
     type: 'FeatureCollection',
     features: []
@@ -29,11 +32,83 @@ export default class PointsController extends Controller {
   }
 
   @action
-  async updateMarkers() {
+  onMove(e) {
+    var coords = e.lngLat;
+    // Set a UI indicator for dragging.
+    this.canvas.style.cursor = 'grabbing';
+    const newPoints = this.markers.features;
+    newPoints[this.hoverdPoint].geometry.coordinates = [coords.lng, coords.lat];
+    this.markers = {
+      type: 'FeatureCollection',
+      features: newPoints,
+    }
+  }
+
+  @action
+  async onUp(e) {
+    const dragedPoint = this.markers.features[this.hoverdPoint];
+    this.canvas.style.cursor = '';
+    this.map.setPaintProperty('point', 'circle-color', 'red');
+    // Unbind mouse/touch events
+    this.map.off('mousemove', this.onMove);
+    this.map.off('touchmove', this.onMove);
+    let point = await this.store.findRecord('point', dragedPoint.pointId);
+    point.longitude = dragedPoint.geometry.coordinates[0];
+    point.latitude =  dragedPoint.geometry.coordinates[1];
+    await point.save();
+  }
+  @action
+  async loadMarkers(map) {
+    this.map = map;
+    this.canvas = map.getCanvasContainer();
     let features = [];
-    await this.allPoints.map(point => {  
+    await this.allPoints.map((point, idx) => {  
       features.pushObject({
         type: 'Feature',
+        id: idx,
+        pointId: point.id,
+        geometry: { type: 'Point', coordinates: [ point.longitude, point.latitude ] },
+        properties: { name: point.name }
+      });
+    });
+    this.markers = {
+      type: 'FeatureCollection',
+      features,
+    }
+    // draggable marker
+    this.map.on('mouseenter', 'point', (e) => {
+      this.hoverdPoint = e.features[0].id
+      this.map.setPaintProperty('point', 'circle-color', '#e45f5f');
+      this.canvas.style.cursor = 'move';
+    });
+    this.map.on('mouseleave', 'point', () => {
+      this.map.setPaintProperty('point', 'circle-color', 'red');
+      this.canvas.style.cursor = '';
+    });
+         
+    this.map.on('mousedown', 'point', (e) => {
+      e.preventDefault(); // Prevent the default map drag behavior.
+      this.canvas.style.cursor = 'grab';
+      this.map.on('mousemove', this.onMove);
+      this.map.once('mouseup', this.onUp);
+    });
+
+    this.map.on('touchstart', 'point', (e) => {
+    if (e.points.length !== 1) return;
+      e.preventDefault(); // Prevent the default map drag behavior.
+      map.on('touchmove', this.onMove);
+      map.once('touchend', this.onUp);
+    });
+  }
+
+  @action
+  async updateMarkers() {
+    let features = [];
+    await this.allPoints.map((point, idx) => {  
+      features.pushObject({
+        type: 'Feature',
+        id: idx,
+        pointId: point.id,
         geometry: { type: 'Point', coordinates: [ point.longitude, point.latitude ] },
         properties: { name: point.name }
       });
